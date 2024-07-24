ff16243d4cda0985d7351d535bc207658d3e1b77
zhangyucs <edwillzy@hotmail.com>
2024-07-24 22:48:12 +0800

    Update processMergeTs.py

--- merge_ts/processMergeTs.py
+++ merge_ts/processMergeTs.py
@@ -3,6 +3,7 @@ import subprocess
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import uuid
 
+
 def read_local_m3u8(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
@@ -12,15 +13,12 @@ def read_local_m3u8(file_path):
 def merge_ts_files(ffmpeg_path, m3u8_path, output_mp4):
     ts_files = read_local_m3u8(m3u8_path)
     temp_filelist = f"filelist_{uuid.uuid4().hex}.txt"
-    
     sp1, sp2 = os.path.split(m3u8_path)
     with open(temp_filelist, 'w') as file:
         for ts in ts_files:
             file.write(f"file '{os.path.join(sp1, ts)}'\n")
-    
     command = [ffmpeg_path, '-f', 'concat', '-safe', '0', '-i', temp_filelist, '-c', 'copy', output_mp4]
     subprocess.run(command)
-    
     os.remove(temp_filelist)
 
 def process_m3u8_file(ffmpeg_path, m3u8_path, output_dir):
@@ -31,12 +29,12 @@ def process_m3u8_file(ffmpeg_path, m3u8_path, output_dir):
 def main(ffmpeg_path, m3u8_paths, output_dir, max_workers):
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
         futures = [executor.submit(process_m3u8_file, ffmpeg_path, m3u8_path, output_dir) for m3u8_path in m3u8_paths]
-        
         for future in as_completed(futures):
             try:
                 future.result()
             except Exception as e:
                 print(f"Error occurred: {e}")
+
 
 if __name__ == '__main__':
     ffmpeg_path = './ffmpeg.exe'
