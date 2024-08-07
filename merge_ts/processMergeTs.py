e136cca7423f1abb28fd7d1c706e3203cabd7bcc
zhangyucs <edwillzy@hotmail.com>
2024-08-07 16:47:49 +0800

    Update processMergeTs.py

--- merge_ts/processMergeTs.py
+++ merge_ts/processMergeTs.py
@@ -38,12 +38,10 @@ def main(ffmpeg_path, m3u8_paths, output_dir, max_workers):
 
 if __name__ == '__main__':
     ffmpeg_path = './ffmpeg.exe'
-    m3u8_paths = [
-        '1.m3u8/index.m3u8', '2.m3u8/index.m3u8', '3.m3u8/index.m3u8', 
-        '4.m3u8/index.m3u8', '5.m3u8/index.m3u8', '6.m3u8/index.m3u8', 
-        '7.m3u8/index.m3u8', '8.m3u8/index.m3u8', '9.m3u8/index.m3u8', 
-        '10.m3u8/index.m3u8', '11.m3u8/index.m3u8', '12.m3u8/index.m3u8'
-    ]
+    paths = ['1.m3u8', '2.m3u8', '3.m3u8', '4.m3u8', 
+         '5.m3u8', '6.m3u8', '7.m3u8', '8.m3u8', 
+         '9.m3u8', '10.m3u8', '11.m3u8', '12.m3u8']
+    m3u8_paths = list(map(lambda x: x + '/index.m3u8', paths))
     output_dir = 'output'
     max_workers = 3
 
